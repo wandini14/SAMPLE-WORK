86adde2df69098a48ac3ec505a789afa2b14f844
wandini14 <106502763+wandini14@users.noreply.github.com>
2022-06-08 15:02:53 +0800

    Update try1.py

--- try1.py
+++ try1.py
@@ -1,18 +1,11 @@
 import streamlit as st
-
 import numpy as np
 import pandas as pd
 
 st.header("My first Streamlit App")
-st.title("This is a title")
 
-st.write("""
-# This is a first-level heading
-## This is a second-level heading
-Hello, this is my first web app. Welcome!!!
-""")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
+st.line_chart(chart_data)
