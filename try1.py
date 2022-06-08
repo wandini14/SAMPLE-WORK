b02810bfa20f63ebec1a0b6e1c5da8756c1391c8
wandini14 <106502763+wandini14@users.noreply.github.com>
2022-06-08 15:57:17 +0800

    Update try1.py

--- try1.py
+++ try1.py
@@ -1,11 +1,12 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 
 st.header("My first Streamlit App")
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
